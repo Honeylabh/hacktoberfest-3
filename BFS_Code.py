26b017412f9b6017a5c373bfa2341a723369103e
Honey <labhhoney@gmail.com>
2024-10-08 18:37:11 +0530

    Create BFS_Code.py

---
+++ BFS_Code.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+
+    while queue:
+        node = queue.popleft()
+        print(node)
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+# Example usage
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+print("BFS traversal starting from node 'A':")
+bfs(graph, 'A')
